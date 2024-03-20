af0a4c339ddf04dc0bf2dcdfa05154dd69f437bf
ShampooBandit <theshampoobandit@gmail.com>
2024-03-20 04:48:52 -0700

    Added repeating actions while key is held down

--- engine.py
+++ engine.py
@@ -6,6 +6,8 @@ import map
 
 class Engine:
     def __init__(self):
+        self.repeat_timer = -1
+        self.repeat_key = -1
         self.camera = pygame.Rect(0, 0, 40, 40)
         self.player = actor.Player(gfx.PLAYER_SPRITES[0], (32, 32))
         self.dungeon = map.Map()
@@ -21,7 +23,13 @@ class Engine:
             self.player.setPosition(self.dungeon.getRandomRoomPosition(self.rng))
             self.updateCamera()
             self.dungeon.updateVisible(self.player.position, self.player.vision_radius, self.camera)
+        
+        self.repeat_timer = 20
+        self.repeat_key = key
 
+        self.handlePlayerAction(key)
+
+    def handlePlayerAction(self, key):
         took_action = self.player.handleInput(key, self.dungeon)
 
         if took_action:
@@ -34,7 +42,19 @@ class Engine:
     def updateCamera(self):
         self.camera[0] = max(0, min(self.player.position[0] - (self.camera[2] / 2), self.dungeon.width - self.camera[2]))
         self.camera[1] = max(0, min(self.player.position[1] - (self.camera[3] / 2), self.dungeon.height - self.camera[3]))
-        self.player.adjustToCamera(self.camera)   
+        self.player.adjustToCamera(self.camera)
+
+    def update(self):
+        if not self.repeat_timer == -1:
+            self.repeat_timer -= 1
+            if self.repeat_timer == 0:
+                self.repeat_timer = 3
+                self.handlePlayerAction(self.repeat_key)
+
+    def clearRepeatTimer(self, key):
+        if key == self.repeat_key:
+            self.repeat_timer = -1
+            self.repeat_key = -1
 
     def draw(self, screen_surface):
         self.dungeon.draw(screen_surface)
--- main.py
+++ main.py
@@ -14,16 +14,21 @@ running = True
 game_engine = engine.Engine()
 
 while running:
+    screen_surface.fill(pygame.Color(0,30,30))
+
     for event in pygame.event.get():
+        text = font.render(str(event.type), False, pygame.Color(255,255,255))
         match event.type:
             case pygame.QUIT:
                 running = False
             case pygame.KEYDOWN:
                 game_engine.handleInput(event.key)
+            case pygame.KEYUP:
+                game_engine.clearRepeatTimer(event.key)
             case _:
                 pass
 
-    screen_surface.fill(pygame.Color(0,30,30))
+    game_engine.update()
 
     game_engine.draw(screen_surface)
 
